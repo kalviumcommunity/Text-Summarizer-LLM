5a12402813cad96baef1ad570a3258309aea0fab
Shankar <lakshmi7708671565@gmail.com>
2025-08-23 22:38:45 +0530

    Completed Chain of thoughts

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from Prompts.zero_shot import summarize_zero_shot
 from Prompts.one_shot import summarize_one_shot
 from Prompts.multi_shot import summarize_multi_shot
+from Prompts.chain_of_thought import summarize_chain_of_thought
 
 CYAN = "\033[96m"
 RED = "\033[91m"
@@ -10,7 +11,7 @@ RESET = "\033[0m"
 def main():
     print(f"\n{GREEN}=== Text Summarizer using LLMs ==={RESET}")
     
-    choice = input("Choose prompting method (zero / one / multi): ").strip().lower()
+    choice = input("Choose prompting method (zero / one / multi / chain): ").strip().lower()
     
     text = input("\nEnter the text to summarize:\n")
     
@@ -23,6 +24,8 @@ def main():
         result = summarize_one_shot(text, length, mode)
     elif choice == "multi":
         result = summarize_multi_shot(text, length, mode)
+    elif choice == "chain":
+        result = summarize_chain_of_thought(text, length, mode)
     else:
         print("Invalid choice. Try again.")
         return
